0d60efb07deb0461cacce2f625ae010fcaefc590
sandeepkiranjada <36130965+sandeepkiranjada@users.noreply.github.com>
2020-05-01 12:40:36 -0400

    Final Avg Model
    Petrubations
    - J2 as in Gurfil
    - Lunisolar SAOD and DOAD Gurfil With Real JPL Ephemeris
    - Lunisolar TOAD with cirular orbit approx  from Gurfil
    - No Drag in this comit

---
+++ FinalProj/Code/Perturbations/lunisolar.m
@@ -0,0 +1,113 @@
+function [ dxdt_lunisol ] = lunisolar(t,x,mu,~,~,~,~,avg_flag,Mjd_UTC_Epoch)
+%LUNISOLAR Summary of this function goes here
+%   Detailed explanation goes here
+global eopdata
+my_constants
+
+MJD_UTC = Mjd_UTC_Epoch+t/86400;
+[UT1_UTC,TAI_UTC] = IERS(eopdata,MJD_UTC,'l');
+[~,~, ~, TT_UTC, ~] = timediff(UT1_UTC,TAI_UTC);
+MJD_TT = MJD_UTC+TT_UTC/86400;
+MJD_TDB = Mjday_TDB(MJD_TT);
+
+[~,d_moon_vec_ECI,d_sun_vec_ECI] = JPL_Eph_DE430(MJD_TDB);
+
+%%% Define H, Hhat, e, ehat from results of integration step
+H_vec = [x(1) x(2) x(3)]'; % Angular momentum  
+H = norm(H_vec);           % Magnitude of angular momentum
+e_vec = [x(4) x(5) x(6)]'; % Eccentricity vector
+e = norm(e_vec);           % Eccentricity 
+
+%%% Define other parameters in terms of state
+a = H^2/(mu*(1-e^2));           % Keplerian semi-major axis
+h_vec = H_vec./sqrt(mu*a);
+n = sqrt(mu/a^3);
+
+d_sun = norm(d_sun_vec_ECI);
+d_sun_hat_vec_ECI = d_sun_vec_ECI/d_sun;
+d_moon = norm(d_moon_vec_ECI);
+d_moon_hat_vec_ECI = d_moon_vec_ECI/d_moon;
+
+
+if avg_flag == 1
+    
+    
+    
+H_dot_sol_vec =  (3*a^2*mu_sun)/( 2*d_sun^3) * ...
+                 ( 5*(d_sun_hat_vec_ECI'*e_vec)*cross(e_vec,d_sun_hat_vec_ECI) - ...
+                  (d_sun_hat_vec_ECI'*h_vec)*cross(h_vec,d_sun_hat_vec_ECI));
+
+H_dot_lun_vec =  (3*a^2*mu_moon)/( 2*d_moon^3) * ...
+                 ( 5*(d_moon_hat_vec_ECI'*e_vec)*cross(e_vec,d_moon_hat_vec_ECI) - ...
+                  (d_moon_hat_vec_ECI'*h_vec)*cross(h_vec,d_moon_hat_vec_ECI));
+
+e_dot_sol_vec =  (3*mu_sun)/( 2*n*d_sun^3) * ...
+                  ( 5*(d_sun_hat_vec_ECI'*e_vec)*cross(h_vec,d_sun_hat_vec_ECI) - ...
+                    (d_sun_hat_vec_ECI'*h_vec)*cross(e_vec,d_sun_hat_vec_ECI) - ...
+                    2*cross(h_vec,e_vec) );
+                
+e_dot_lun_vec =  (3*mu_moon)/( 2*n*d_moon^3) * ...
+                  ( 5*(d_moon_hat_vec_ECI'*e_vec)*cross(h_vec,d_moon_hat_vec_ECI) - ...
+                    (d_moon_hat_vec_ECI'*h_vec)*cross(e_vec,d_moon_hat_vec_ECI) - ...
+                    2*cross(h_vec,e_vec) );
+                
+                
+elseif avg_flag == 2
+    
+    
+    
+H_dot_sol_vec =  (3*a^2*mu_sun)/( 2*d_sun^3) * ...
+                 ( 5*(d_sun_hat_vec_ECI'*e_vec)*cross(e_vec,d_sun_hat_vec_ECI) - ...
+                  (d_sun_hat_vec_ECI'*h_vec)*cross(h_vec,d_sun_hat_vec_ECI));
+
+H_dot_lun_vec = -(3*a^2*mu_moon)/( 4*a_moon^3 * h_moon^3 ) * ...
+                 ( 5*(e_vec'*H_moon_hat_vec)*cross(e_vec,H_moon_hat_vec) - ...
+                  (h_vec'*H_moon_hat_vec)*cross(h_vec,H_moon_hat_vec));
+
+e_dot_sol_vec =  (3*mu_sun)/( 2*n*d_sun^3) * ...
+                  ( 5*(d_sun_hat_vec_ECI'*e_vec)*cross(h_vec,d_sun_hat_vec_ECI) - ...
+                    (d_sun_hat_vec_ECI'*h_vec)*cross(e_vec,d_sun_hat_vec_ECI) - ...
+                    2*cross(h_vec,e_vec) );
+                
+e_dot_lun_vec  = -(3*mu_moon)/( 4*n*a_moon^3*h_moon^3) * ...
+                  ( 5*(H_moon_hat_vec'*e_vec)*cross(h_vec,H_moon_hat_vec) - ...
+                    (h_vec'*H_moon_hat_vec)*cross(e_vec,H_moon_hat_vec) - ...
+                    2*cross(h_vec,e_vec) );    
+                
+elseif avg_flag == 3
+    
+H_dot_sol_vec = -(3*a^2*mu_sun)/( 4*a_sun^3 * h_sun^3 ) * ...
+                 ( 5*(e_vec'*H_sun_hat_vec)*cross(e_vec,H_sun_hat_vec) - ...
+                  (h_vec'*H_sun_hat_vec)*cross(h_vec,H_sun_hat_vec));
+
+H_dot_lun_vec = -(3*a^2*mu_moon)/( 4*a_moon^3 * h_moon^3 ) * ...
+                 ( 5*(e_vec'*H_moon_hat_vec)*cross(e_vec,H_moon_hat_vec) - ...
+                  (h_vec'*H_moon_hat_vec)*cross(h_vec,H_moon_hat_vec));
+
+e_dot_sol_vec  = -(3*mu_sun)/( 4*n*a_sun^3*h_sun^3) * ...
+                  ( 5*(H_sun_hat_vec'*e_vec)*cross(h_vec,H_sun_hat_vec) - ...
+                    (h_vec'*H_sun_hat_vec)*cross(e_vec,H_sun_hat_vec) - ...
+                    2*cross(h_vec,e_vec) );
+                
+e_dot_lun_vec  = -(3*mu_moon)/( 4*n*a_moon^3*h_moon^3) * ...
+                  ( 5*(H_moon_hat_vec'*e_vec)*cross(h_vec,H_moon_hat_vec) - ...
+                    (h_vec'*H_moon_hat_vec)*cross(e_vec,H_moon_hat_vec) - ...
+                    2*cross(h_vec,e_vec) );    
+                
+                
+                
+else
+    error('Incorrect Value of Wang et al. averaging flag. Set avg_flag to: 1, 2, or 3')
+end
+
+
+%%% Differential equations for angular momentum (H) and eccentricity vector (e)
+dHvec = H_dot_sol_vec+H_dot_lun_vec;
+devec = e_dot_sol_vec+e_dot_lun_vec;
+
+%%% Save differential equations to output variable for function
+dxdt_lunisol = [dHvec;devec];
+
+
+end
+
---
+++ FinalProj/Code/Perturbations/my_constants.m
@@ -0,0 +1,33 @@
+%% Oblateness
+
+J2 = 1.08263e-3;
+
+
+
+%% Luni-Solar Constants For TAOD
+inc = 23.44*pi/180;
+
+a_sun = 149597870700; % Mean value used, circular orbit assumption
+V_mean_earth = 29.785731561564607e3;
+H_sun_vec = V_mean_earth*a_sun.*[0 -sin(inc) cos(inc)]';
+H_sun_hat_vec = [0 -sin(inc) cos(inc)]';
+mu_sun = 1.32712440018e20;
+h_sun_vec = H_sun_vec./sqrt(a_sun*mu_sun);
+h_sun = norm(h_sun_vec);
+n_sun = sqrt(mu_sun/a_sun^3);
+
+a_moon = 385e6; % Mean value used, circular orbit assumption
+V_mean_moon = 1.022e3;
+H_moon_vec = V_mean_moon*a_moon.*[0 -sin(inc) cos(inc)]';
+H_moon_hat_vec = [0 -sin(inc) cos(inc)]';
+mu_earth = 3.986004418e14;
+h_moon_vec = H_moon_vec./sqrt(a_moon*mu_earth);
+h_moon = norm(h_moon_vec);
+n_moon = sqrt(mu_earth/a_moon^3);
+
+
+mu_moon = mu_earth/81.30056907419062; %4.9048695e12;
+
+
+
+
