26d714ada85fcaf97de22a1f82eff9de8dc24dbb
AH2020VT <64446114+AH2020VT@users.noreply.github.com>
2020-05-03 21:40:10 -0400

    5_3_2020

--- FinalProj/Code/Data/ICGURFIL.m
+++ FinalProj/Code/Data/ICGURFIL.m
@@ -30,6 +30,7 @@ M0 = 0;                                     % Initial mean anomaly [rad]
 p0 = a0*(1-e0^2);                           %- semilatus rectum              m
 nu0 = 0;
 [r0,v0]  = coe2rv(p0/1e3,e0,i0,raan0,argp0,nu0);
+r0=r0*1e3;v0=v0*1e3;
 H0 = sqrt(a0*mu_earth*(1-e0^2));            % Initial angular momentum
 
 %
